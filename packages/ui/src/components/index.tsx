7ea9519d66266969d69c302d5b75551f711047f1
EmoPorEmilio <franceschini.emilio@gmail.com>
2024-08-08 16:15:20 -0300

    update

---
+++ packages/ui/src/components/index.tsx
@@ -0,0 +1,6 @@
+import LateralNav from './Header/LateralNav'
+import Header from './Header'
+import { Alert, AlertType } from './Alert'
+import { Button, ButtonType } from './Button';
+import Chip from './Chip';
+export { LateralNav, Header, Alert, AlertType, Button, ButtonType, Chip }
